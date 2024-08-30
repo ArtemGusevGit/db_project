b178ca87d86cbfdcc318370d651bc5f741a3e3f7
ArtemGusev <agusev@animaccord.dev>
2024-08-30 14:06:15 +0300

    fix: fix styles on user-update.php

--- user-update.php
+++ user-update.php
@@ -39,12 +39,11 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     <title>Demo DB</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles/style.css">
 </head>
 <body>
 
 <section class="p-4">
-    <form action="includes/user-update.inc.php" class="p-3" method="post">
+    <form action="includes/user-update.inc.php" method="post">
         <h3>Update</h3>
 
         <div class="form-group">
